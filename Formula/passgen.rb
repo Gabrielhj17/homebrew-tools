7f96e760b495ee530e6a582abe8fc307f633b66f
Gabrielhj17 <gabrielhj@icloud.com>
2025-01-15 00:11:43 +0000

    Bump passgen version to 1.0.1

--- Formula/passgen.rb
+++ Formula/passgen.rb
@@ -2,7 +2,7 @@ class Passgen < Formula
   desc "Simple command-line password generator"
   homepage "https://github.com/Gabrielhj17/PassGen"
   url "https://raw.githubusercontent.com/Gabrielhj17/PassGen/main/passgen-release.tar.gz"
-  version "1.0.0"
+  version "1.0.1"
   sha256 "7bbc8ca3ca5289d4a0c6ffd32514b8fa98fa0e588923dcb1a3009e838cbd0853"
   license "MIT"
 
