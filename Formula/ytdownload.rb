811d734276827862ce7596f85466b58602d55410
Gabrielhj17 <56936967+Gabrielhj17@users.noreply.github.com>
2025-01-21 21:01:49 +0000

    Update sha 256 (again)

--- Formula/ytdownload.rb
+++ Formula/ytdownload.rb
@@ -5,14 +5,14 @@ class Ytdownload < Formula
   homepage "https://github.com/Gabrielhj17/YTdownload"
   url "https://github.com/Gabrielhj17/YTdownload/raw/refs/heads/main/ytdownload-release.tar.gz"
   version "1.0.0"
-  sha256 "59b638d6fa98b82bdb903df10c09cfe60057841b32937a2ff0da9978eb0916ea"
+  sha256 "9cf46d6a916801250fc167d212566008a5aee0165c5101cdd0b4915e4a53334a"
   license "MIT"
 
   depends_on "python@3.11"
 
   resource "yt-dlp" do
     url "https://files.pythonhosted.org/packages/fd/e9/61fc5947d35e9fcdd8efca88a5a2de53946a4aaf4e6c3e23f7b1b721b4c8/yt_dlp-2025.1.15.tar.gz"
-    sha256 "59b638d6fa98b82bdb903df10c09cfe60057841b32937a2ff0da9978eb0916ea"
+    sha256 "e8ec515d49bb62704915d13a22ee6fe03a5658d651e4e64574e3a17ee01f6e3b"
   end
 
   def install
